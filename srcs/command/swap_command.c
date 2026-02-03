1178459a6db4a4bb7544b1c27c32ed07e1bae430
yuttto0024 <yuto0024@outlook.com>
2026-02-03 14:55:55 +0900

    feat/swap_command.c

---
+++ srcs/command/swap_command.c
@@ -0,0 +1,20 @@
+#include "push_swap.h"
+
+void	sa(t_stack *a)
+{
+	swap(a);
+	ft_printf("sa\n");
+}
+
+void	sb(t_stack *b)
+{
+	swap(a);
+	ft_printf("sa\n");
+}
+
+void	ss(t_stack *a, t_stack *b)
+{
+	swap(a);
+	swap(b);
+	ft_printf("ss\n");
+}
